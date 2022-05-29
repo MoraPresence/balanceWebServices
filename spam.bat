c8e17eae6dc76c9ec40409f4abc184559dc54503
MoraPresence <54858588+MoraPresence@users.noreply.github.com>
2022-05-29 15:10:22 +0300

    Add files via upload

--- spam.bat
+++ spam.bat
@@ -1,3 +1,22 @@
-:loop
+set /A Counter=0
+:WORK
 curl http://127.0.0.1:8080
-goto loop
+timeout 1 > nul
+set /A Counter+=1
+if %Counter% equ 100 (
+	goto :offTarget
+)
+if %Counter% equ 200 (
+	goto :onTarget
+)
+goto :WORK
+ 
+:offTarget
+echo %Counter%
+goto :WORK
+
+:onTarget
+echo %Counter%
+goto :WORK
+ 
+exit /b
